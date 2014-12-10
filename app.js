aad8cdc729e6d1e163a698407a2406a093a15f94
Cristhian Escobar <cristhian.escobar@mutualmobile.com>
2014-12-10 08:52:51 -0600

    Updated views

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var path = require("path");
 var app = express();
 
 app.set('views', __dirname + '/views');
-app.use(express.static(path.join(__dirname, 'static')));
-app.use('static/css', express.static(path.join(__dirname, 'static/css')));
-app.use('static/font', express.static(path.join(__dirname, 'static/font')));
-app.use('static/img', express.static(path.join(__dirname, 'static/img')));
-app.use('static/js', express.static(path.join(__dirname, 'static/js')));
+app.use(express.static(path.join(__dirname, '/static')));
+app.use('static/css', express.static(path.join(__dirname, '/static/css')));
+app.use('static/font', express.static(path.join(__dirname, '/static/font')));
+app.use('static/img', express.static(path.join(__dirname, '/static/img')));
+app.use('static/js', express.static(path.join(__dirname, '/static/js')));
 app.engine('html', require('ejs').renderFile);
 
 
