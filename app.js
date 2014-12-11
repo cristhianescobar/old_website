e5ff24d9c823bbf7da5695578333a5429cc46791
Cristhian Escobar <cristhian.escobar@mutualmobile.com>
2014-12-10 23:34:17 -0600

    Added icon

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var app = express();
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var favicon  = require('serve-favicon')
 
+
+app.use(favicon(__dirname + '/static/img/favicon.ico'))
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, '/static')));
 app.use('static/css', express.static(path.join(__dirname, '/static/css')));
